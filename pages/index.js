e2c1e22371a75a7281b85ff5153a5babb26f8375
Edgar Hovhannisyan <edgarwyn@gmail.com>
2022-11-05 10:52:15 +0400

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import coingeckoApi from 'coingecko-api';
+// import coingeckoApi from 'coingecko-api';
+import bitcoin from '/public/coin.png'
 import CoinGecko from 'coingecko-api/lib/CoinGecko';
 const coinGeckoClient = new CoinGecko();
 
@@ -41,17 +42,34 @@ export default function Home(props) {
         <div className='text'>
            <h1>Coinfo Club</h1> <br /><br />
        <h2>The best online platform, where you can find the latest crypto-related news, prices and also buy | sell coins at one place</h2>
+       <a href="#prices" className='rateButton'>Latest rates</a>
         </div>
-        {/* <Image src='/blk.png' width={'100%'} height={'100%'}> */}
-        {/* <div className='image'>
-          // <img src='/blk.png'/>
-        </div> */}
-      {/* </Image> */}
-      {/* <img
-                  src={blk.png}
-                  style={{width: 400, height: 400}}
-                /> */}
-               
+
+        {/* <div id="ui">
+  <div class="crystal">
+    <div class="crystal_panel top front"></div>
+    <div class="crystal_panel top back"></div>
+    <div class="crystal_panel top left"></div>
+    <div class="crystal_panel top right"></div>
+    <div class="crystal_panel bottom front"></div>
+    <div class="crystal_panel bottom back"></div>
+    <div class="crystal_panel bottom left"></div>
+    <div class="crystal_panel bottom right"></div>
+  </div>
+</div> */}
+     
+    <div className='imgBody'>
+    <Image
+      src={bitcoin}
+      alt="coin image"
+      width=""
+      height=""
+    />
+
+    </div>
+    
+  
+    
         
 
      
@@ -65,14 +83,16 @@ export default function Home(props) {
        */}
       
 
-      <iframe width="100%" scrolling="yes"   src="https://cryptopanic.com/widgets/news/?bg_color=142035&amp;font_family=sans&amp;header_bg_color=3071e7&amp;header_text_color=eee&amp;link_color=3071e7&amp;news_feed=trending&amp;posts_limit=10&amp;text_color=eee&amp;title=Latest%20Trending%20News%20of%20Crypto%20World%20in%20Coinfo.club" height="900px"></iframe>
+      <iframe width="100%" scrolling="no" className='news'  src="https://cryptopanic.com/widgets/news/?bg_color=142035&amp;font_family=sans&amp;header_bg_color=3071e7&amp;header_text_color=eee&amp;link_color=3071e7&amp;news_feed=trending&amp;posts_limit=10&amp;text_color=eee&amp;title=Latest%20Trending%20News%20of%20Crypto%20World%20in%20Coinfo.club" height="770px" ></iframe>
+
+     
 
     
 
       </div>
 
 
-      {/* <div className='exchange'>
+      <div className='exchange'>
 
       <div className='exchangeText'>       
       <h3>Limitless Crypto Exchange</h3> 
@@ -85,7 +105,7 @@ export default function Home(props) {
     <script defer type='text/javascript' src='https://changenow.io/embeds/exchange-widget/v2/stepper-connector.js'></script>
         </div>
 
-      </div>  */}
+      </div> 
 
       
      
@@ -98,7 +118,7 @@ export default function Home(props) {
 
       
 <h3>Crypto Chart</h3><br />
-      <table className='table'>
+      <table className='table' id='prices'>
         
         <thead>
           
@@ -106,7 +126,7 @@ export default function Home(props) {
             <th>Symbol</th>
             <th>Price</th>
             <th>Change</th>            
-            <th>Market</th>
+            {/* <th>Market</th> */}
           </tr>          
         </thead>  
         <tbody>
@@ -129,7 +149,7 @@ export default function Home(props) {
                 </span>
               </td>
               
-              <td>{formatDollar (coin.market_cap, 3)}</td>
+              {/* <td>{formatDollar (coin.market_cap, 3)}</td> */}
             </tr>
           ))}
         </tbody>
