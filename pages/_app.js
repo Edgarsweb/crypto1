973fddd406712a8764f680e011a04cee694c7605
Edgar Hovhannisyan <edgarwyn@gmail.com>
2023-01-30 02:04:47 +0400

    edits

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Script
+  id="Adsense-id"
+  data-ad-client="ca-pub-7904408176093356"
+  async="true"
+  strategy="beforeInteractive"
+   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+/>
+<Component {...pageProps} /></>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import Script from 'next/script'
+
 import styles from '../styles/Home.module.css'
 // import coingeckoApi from 'coingecko-api';
 import bitcoin from '/public/coin.png'
@@ -34,13 +34,7 @@ export default function Home(props) {
       
 
 
-{/* <Script
-   id="Adsense-id"
-   data-ad-client="ca-pub-7904408176093356"
-   async="true"
-   strategy="beforeInteractive"
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-/> */}
+
       <Head>
         <title>Cryptocurrency platform | Coinfo Club</title>
         <meta name="description" content="The largest online crypto community in the world, where you can find the latest rates of different currencies and crypto" />
